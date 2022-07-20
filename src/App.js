9332542ceb3403ccf77a8624955770d0b5f67cb6
Correy Lim <correylim@gmail.com>
2022-07-20 14:36:18 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useRef, useEffect } from 'react';
+import WebViewer from '@pdftron/webviewer';
+import './App.css';
+
+const App = () => {
+  const viewer = useRef(null);
+
+  // if using a class, equivalent of componentDidMount 
+  useEffect(() => {
+    WebViewer(
+      {
+        path: '/webviewer/lib',
+        initialDoc: '/files/PDFTRON_about.pdf',
+      },
+      viewer.current,
+    ).then((instance) => {
+
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="header">React sample</div>
+      <div className="webviewer" ref={viewer}></div>
+    </div>
+  );
+};
+
+export default App;
